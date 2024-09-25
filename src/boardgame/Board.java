36ae7c0451796c9742db781845d5fc71e24b4b06
BraianMelo <Braian.ptu@gmail.com>
2024-09-25 09:36:09 -0300

    Rei e Torre implementadas

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,11 +36,12 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
-	@Override
-	public String toString() {
-		return "board";
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 	
+
 	
 	
 	
