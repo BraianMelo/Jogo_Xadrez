7d59449e77f56e6bfaaf0a3358cf59339fa2758d
BraianMelo <Braian.ptu@gmail.com>
2024-09-27 20:56:46 -0300

    Movimentação de peças

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -24,6 +28,17 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static ChessPosition readChessPosition(Scanner sc) {
+		
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
+			throw new InputMismatchException("Erro lendo posição: valores válidos são de a1 até h8.");
+		}
+	}
 	
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -46,7 +61,7 @@ public class UI {
 	
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("- ");
 		
 		}  else {
             if (piece.getColor() == Color.WHITE) {
@@ -62,6 +77,7 @@ public class UI {
 		
 		}
 	}
+	
 	
 	
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -61,6 +61,22 @@ public class Board {
 		
 		return piece(position) != null;
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Essa posição não existe!");
+		}
+		
+		if (piece(position) == null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+		
+	}
 
 	
 	
