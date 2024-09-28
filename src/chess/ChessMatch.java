725758b964bd084b403f981e9b2e78d8b14d94b6
BraianMelo <Braian.ptu@gmail.com>
2024-09-27 23:14:27 -0300

    Movimentos possiveis

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -46,7 +46,10 @@ public class ChessMatch {
 	
 	private void validadeSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position))
-			throw new ChessException("Não existe peça na posição de origem");
+			throw new ChessException("Não existe peça na posição de origem.");
+		
+		if (!board.piece(position).isThereAnyPossibleMove())
+			throw new ChessException("Não existe movimentos possíveis para a peça escolhida!");
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
