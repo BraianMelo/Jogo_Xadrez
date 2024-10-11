52f2d42d57be1858cd1c0a9a8900d53fede610da
BraianMelo <Braian.ptu@gmail.com>
2024-10-11 16:14:20 -0300

    Movimentos possiveis da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,7 @@
 package chess;
 
+import javax.print.attribute.standard.Destination;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -33,6 +35,7 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 		
 		validadeSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -50,6 +53,12 @@ public class ChessMatch {
 		
 		if (!board.piece(position).isThereAnyPossibleMove())
 			throw new ChessException("Não existe movimentos possíveis para a peça escolhida!");
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
