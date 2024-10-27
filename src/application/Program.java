3c83f62fd6db2743237e4f7d2896c3d5cef8f0cd
BraianMelo <Braian.ptu@gmail.com>
2024-10-27 10:44:10 -0300

    Promoção corrijida

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,7 +43,13 @@ public class Program {
 				
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Digite a peça promovida (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) { 
+						System.out.println("Peça inválida! Escolha uma peça válida (B/N/R/Q)!");
+						System.out.print("Digite a peça promovida (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 				
